1097def244da44c4084f1ca258a89827520da4ca
groupguanfang <1203970284@qq.com>
2022-09-18 20:27:40 +0800

    9/18

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'HomeView',
-    component: () => import('../views/HomeView.vue')
+    path: "/",
+    name: "HomeView",
+    component: () => import("../views/HomeView.vue"),
   },
   {
-    path: '/user',
-    name: 'UserView',
-    component: () => import('../views/UserView.vue')
-  }
-]
+    path: "/user",
+    name: "UserView",
+    component: () => import("../views/UserView.vue"),
+  },
+];
 
-const router = new VueRouter({
-  routes
-})
+const router = new VueRouter({ routes });
 
-export default router
+export default router;
